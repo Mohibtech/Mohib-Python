95fe45dba13639ad2939dd121581a78c81b8765d
Mohib Alvi <mohibtech@gmail.com>
2017-02-24 16:23:03 +0500

    Update twitter_sentiments.py
    Added function authTwitter()
    Moved tweept search code within csv file writing code
    used minimum variables to write row data in writerow function of csvWriter.

--- twitter_sentiments.py
+++ twitter_sentiments.py
@@ -1,52 +1,40 @@
 # -*- coding: utf-8 -*-
-from textblob import TextBlob
-import unicodecsv as csv
+import csv
 import tweepy
-import unidecode
+import unidecode as ud
 
-# AUTHENTICATION (OAuth)
-#Create auth.k file with fields in following order 
-#consumer key
-#consumer secret
-#access key
-#access secret
-f = open('auth.k','r')
-ak = f.readlines()
-f.close()
-auth1 = tweepy.auth.OAuthHandler(ak[0].replace("\n",""), ak[1].replace("\n",""))
-auth1.set_access_token(ak[2].replace("\n",""), ak[3].replace("\n",""))
-api = tweepy.API(auth1)
+# Twitter auth.k file format
+'''
+consumer_key
+consumer_secret
+access_key
+access_secret
+'''
 
-# Tweeter search with keyword
-target_num = 50
-query = "olympics"
+def authTwitter():               
+        with open('auth.k','r') as authfile:
+                ak = authfile.readlines()
 
-csvFile = open('results_olympics.csv','w', encoding='utf-8')
-csvWriter = csv.writer(csvFile)
-csvWriter.writerow(["username","author id","created", "text", "retwc", "hashtag", "followers", "friends","polarity","subjectivity"])
-counter = 0
+        consumer_key, consumer_secret = ak[0].strip(), ak[1].strip()
+        access_key, access_secret = ak[2].strip(), ak[3].strip()
+        
+        auth1 = tweepy.auth.OAuthHandler(consumer_key, consumer_secret)
+        auth1.set_access_token(access_key, access_secret)
+        return tweepy.API(auth1)
 
-for tweet in tweepy.Cursor(api.search, q = query, lang = "en", result_type = "popular", count = target_num).items():
-    created = tweet.created_at
-    text = tweet.text
-    text = unidecode.unidecode(text) 
-    retwc = tweet.retweet_count
-    try:
-        hashtag = tweet.entities[u'hashtags'][0][u'text'] #hashtags used
-    except:
-        hashtag = "None"
-    username  = tweet.author.name            #author/user name
-    authorid  = tweet.author.id              #author/user ID#
-    followers = tweet.author.followers_count #number of author/user followers (inlink)
-    friends = tweet.author.friends_count     #number of author/user friends (outlink)
+api = authTwitter()
 
-    text_blob = TextBlob(text)
-    polarity = text_blob.polarity
-    subjectivity = text_blob.subjectivity
-    csvWriter.writerow([username, authorid, created, text, retwc, hashtag, followers, friends, polarity, subjectivity])
+with open('test1_tweets.csv', 'w', encoding='utf8') as csvTweets:
+    csvWriter = csv.writer(csvTweets)
+    col_header = ['username','author id','created', 'text', 'retwc', 'hashtag', 'followers', 'friends']
+    csvWriter.writerow(col_header)
 
-    counter = counter + 1
-    if (counter == target_num):
-        break
-
-csvFile.close()
+    for t in tweepy.Cursor(api.search, q="urdu", lang="en", result_type="popular", count=10).items():
+        text = ud.unidecode(t.text)
+        try:
+            hashtag = tweet.entities[u'hashtags'][0][u'text'] #hashtags used
+        except:
+            hashtag = "None"
+        
+        row = [t.author.name, t.author.id , t.created_at, text, t.retweet_count, hashtag, t.author.followers_count, t.author.friends_count]
+        csvWriter.writerow(row)
