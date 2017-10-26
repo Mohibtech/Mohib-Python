e89f2b06d08124422bb2bd565d85bdd9d18194c5
Mohib Alvi <mohibtech@gmail.com>
2017-10-26 10:55:43 +0500

    Create ColorSquareSpiral.py

---
+++ turtle/ColorSquareSpiral.py
@@ -0,0 +1,11 @@
+# Square Spiral with 4 colors
+
+import turtle 
+
+t = turtle.Pen() 
+colors = ["red", "yellow", "blue", "green"]
+
+for x in range(100):
+    t.pencolor(colors[x%4]) 
+    t.forward(x) 
+    t.left(91)
