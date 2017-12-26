1cbb066ddc590b07775491fa21bc19ba2d8a128b
Mohib Alvi <mohibtech@gmail.com>
2017-12-26 17:48:19 +0500

    Update renameFiles.py

--- FileHandling/renameFiles.py
+++ FileHandling/renameFiles.py
@@ -1,12 +1,15 @@
 import os
 
-workFolder = r'C:\MEDIA\TV Seasons\Walking dead'
+workFolder = r'E:\TECH VID\Oracle 12c\Oracle 12c New Features\2 ILM'
 fileList = os.listdir(workFolder)
 os.chdir( workFolder )
 
 for filename in fileList:
     fname, ext = os.path.splitext(filename)
-    seasonEp = fname.split('.')
-    newname = str(seasonEp[3] + '_' + seasonEp[1] + seasonEp[2] + ext)
-    
-    os.rename(filename, newname)
+    fileNamePt = fname.split('-')
+    numFileName = len(fileNamePt)
+    if numFileName > 1:
+        newname = str(fileNamePt[1] + ext)
+        print(newname)
+    else:
+        print(fname, numFileName)
