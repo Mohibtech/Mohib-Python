627ed2926fc798fc693b25b6c940e547e1008b85
Mohib Alvi <mohibtech@gmail.com>
2017-12-29 16:59:53 +0500

    Update renameFiles.py

--- FileHandling/renameFiles.py
+++ FileHandling/renameFiles.py
@@ -1,15 +1,21 @@
 import os
+import tkinter as tk
+from tkinter import filedialog
 
-workFolder = r'E:\TECH VID\Oracle 12c\Oracle 12c New Features\2 ILM'
-fileList = os.listdir(workFolder)
-os.chdir( workFolder )
+# Initializing tkinter
+root = tk.Tk()
+workingDir = filedialog.askdirectory(parent=root,initialdir="/",title='Please select a directory')
+
+fileList = os.listdir(workingDir)
+os.chdir( workingDir )
 
 for filename in fileList:
     fname, ext = os.path.splitext(filename)
     fileNamePt = fname.split('-')
     numFileName = len(fileNamePt)
     if numFileName > 1:
-        newname = str(fileNamePt[1] + ext)
+        newname = str(fileNamePt[1].strip() + ext)
         print(newname)
+        os.rename(filename, newname)
     else:
         print(fname, numFileName)
