13b58f0e644a8b37db783e6bbd96d07e7af0f3e7
Mohib Alvi <mohibtech@gmail.com>
2018-01-17 18:12:46 +0500

    Update renameFiles.py

--- FileHandling/renameFiles.py
+++ FileHandling/renameFiles.py
@@ -4,22 +4,32 @@ from tkinter import filedialog
 
 tkroot = tk.Tk()  # Initializing Tkinter
 workingDir = filedialog.askdirectory(parent=tkroot,initialdir="/",title='Please select a directory')
+#workingDir = r'F:\TV MEDIA\Dirilis Ertugrul\Season 2'
+dramaSeries = " Diris Ertugrul"
 
 fileList = os.listdir(workingDir)
 os.chdir( workingDir )
 
 # Traverse root directory, and list directories as dirs and files as files
 for root, dirs, fileList in os.walk(workingDir):
+    os.chdir( root )
     path = root.split(os.sep)
     print((len(path) - 1) * '..', os.path.basename(root))
     for filename in fileList:
         fname, ext = os.path.splitext(filename)
-        fileNamePt = fname.split('-')
-        numFileName = len(fileNamePt)
-        if numFileName > 1:
-            newname = str(fileNamePt[0].strip() + ext)
+        fileSplit = fname.split('-')
+        #numFileName = len(filePart)
+        if len(fileSplit) > 1:
+            filePart = str(fileSplit[1]).split(' ')
+            newname = str(filePart[8] + " Episode" + dramaSeries + ext)
             print(len(path) * '..', newname)    
-            #os.rename(filename, newname)
+            os.rename(filename, newname)
         else:
-            print(fname, numFileName)
+            print(fname, len(fileSplit))
+
+
+
+    
+    
+        
 
