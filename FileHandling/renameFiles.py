f23830c5355e32efbd946ad4d02319fac0245b5d
Mohib Alvi <mohibtech@gmail.com>
2018-01-26 17:24:09 +0500

    Update renameFiles.py

--- FileHandling/renameFiles.py
+++ FileHandling/renameFiles.py
@@ -6,9 +6,9 @@ tkroot = tk.Tk()  # Initializing Tkinter
 workingDir = filedialog.askdirectory(parent=tkroot,initialdir="/",title='Please select a directory')
 #workingDir = r'F:\TV MEDIA\Dirilis Ertugrul\Season 2'
 dramaSeries = " Diris Ertugrul"
+EpNum = 0
 
 fileList = os.listdir(workingDir)
-os.chdir( workingDir )
 
 # Traverse root directory, and list directories as dirs and files as files
 for root, dirs, fileList in os.walk(workingDir):
@@ -17,17 +17,22 @@ for root, dirs, fileList in os.walk(workingDir):
     print((len(path) - 1) * '..', os.path.basename(root))
     for filename in fileList:
         fname, ext = os.path.splitext(filename)
-        fileSplit = fname.split('-')
-        #numFileName = len(filePart)
-        if len(fileSplit) > 1:
-            filePart = str(fileSplit[1]).split(' ')
-            newname = str(filePart[8] + " Episode" + dramaSeries + ext)
+        # First split based on char '-' , storing the first element in this case
+        fileSplit1 = fname.split('-')[0]
+        if len(fileSplit1) > 1:
+            # second split based on empty space char " "
+            EpName = fileSplit1.split(' ')
+            for i in range(len(EpName)):
+                # Checking every element of splitted string for a digit only to store it as the episode number
+                if EpName[i].isdigit():
+                    EpNum = EpName[i]
+
+            #Episode = str(fileSplit1[0]).split(' ')
+            newname = str(EpNum + dramaName + ext)
             print(len(path) * '..', newname)    
             os.rename(filename, newname)
         else:
-            print(fname, len(fileSplit))
-
-
+            print(fname, len(fileSplit1))
 
     
     
