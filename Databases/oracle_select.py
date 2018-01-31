e387b2486f04acbd350d5971d697394a8151b65e
Mohib Alvi <mohibtech@gmail.com>
2018-01-31 18:38:58 +0500

    Create oracle_select.py

---
+++ Databases/oracle_select.py
@@ -0,0 +1,16 @@
+import cx_Oracle
+
+# Connect as user "hr" with password "welcome" to the "oraclepdb" service running on this computer.
+connection = cx_Oracle.connect("hr", "hr", "localhost/far.genie.com")
+cursor = connection.cursor()
+
+statement="""
+          SELECT first_name, last_name FROM hr.employees 
+          where employee_id > :eid 
+          and department_id=:did
+          """
+cursor.execute(statement, (100,10))
+
+result = cursor.fetchall()
+for row in result:
+    print(row[0],row[1])
