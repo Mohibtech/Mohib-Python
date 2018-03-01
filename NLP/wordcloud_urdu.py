55e727a94ec4911c586d91747072d5afe0fdeb64
Mohib Alvi <mohibtech@gmail.com>
2018-03-01 13:07:35 +0500

    Update wordcloud_urdu.py

--- NLP/wordcloud_urdu.py
+++ NLP/wordcloud_urdu.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
-
-from wordcloud import WordCloud
 import nltk
+import sys
+import unicodedata
 import matplotlib.pyplot as plt
-from arabic_reshaper import arabic_reshaper
-from bidi.algorithm import get_display
+from wordcloud import WordCloud
 
 def word_populate(FILE_NAME):
     try:
@@ -14,29 +13,39 @@ def word_populate(FILE_NAME):
            print(e)
     return bookwords
 
-def remove_stopwords(text):
-    words = nltk.word_tokenize(text)
-    #stopwords_ur = ['میں','اور','کو','کی','کا', 'مجھے', 'پر','اس','جب', 'مگر ', 'کر','کے','کہ','ہو','ہی','اپنی','اپنے','نہیں','ہے','ہوں','گیا','ایک','سے','پھر','کوئی','یہاں','آپ','ہیں','تک','وہاں','ہوں','میرے','ہوا','لے','تھی','گی','اگر']
+def remove_punct(text):
+    remove_punc_map = dict.fromkeys(i for i in range(sys.maxunicode) 
+                                       if unicodedata.category(chr(i)).startswith('P'))
+    return text.translate(remove_punc_map)
+
+def remove_stopwords(words):
+    stopwords_ur = word_populate(r'urdu/stopwords_UR.txt')
     stopwords_removed = [w for w in words if w not in stopwords_ur]
     return " ".join(stopwords_removed)
 
-# Read the whole text.
-text = open('Data/Firdoos.txt','r',encoding='utf8').read()
+# Read whole unicode text 
+text = open('urdu/Firdoos.txt','r',encoding='utf8').read()
 
-text = remove_stopwords(text)
-#text = text.decode("utf-8")
+# Calling function to remove punctuations using unicode codepoints starting with 'P'
+text = remove_punct(text)
+
+# Tokenizing text into words
+words = nltk.word_tokenize(text)
+
+text = remove_stopwords(words)
+
+# Reconstruct Arabic/Urdu sentences to be used in applications that don't support Arabic script.
+from arabic_reshaper import arabic_reshaper
 text = arabic_reshaper.reshape(text)
+
+# For right-to-left text rendering, need to use get_display from python-bidi
+from bidi.algorithm import get_display
 text = get_display(arabic_reshaper.reshape(text))
 
 # Generate a word cloud image
-wordcloud =  WordCloud(font_path='tahoma.ttf').generate(text)
+wordcloud =  WordCloud(font_path='urdu-najd-regular-1.ttf').generate(text)
 
-#wordcloud.to_image()
-
-
-# Display the generated image:
-# the matplotlib way:
-import matplotlib
+# Display the generated image: the matplotlib way
 import matplotlib.pyplot as plt
 
 #matplotlib.rc('font', family='Tahoma')
