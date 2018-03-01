1757952eeb2895e279396e4701351c8e3387ea4a
Mohib Alvi <mohibtech@gmail.com>
2018-03-01 18:02:26 +0500

    Update Unicode webpage read.py

--- Unicode webpage read.py
+++ Unicode webpage read.py
@@ -1,9 +1,11 @@
 #Unicode webpage read		
-import urllib.request as urlreq
+from urllib.request import urlopen
  
 # Copy all of the content from UrduWeb page
-URL = 'http://www.urduweb.org/mehfil/threads/' + \
-'%D9%BE%D8%A7%D8%A6%D8%AA%DA%BE%D9%88%D9%86-%DA%A9%D9%88%D8%B1%D8%B3-%DA%A9%DB%92-%D9%86%D8%A6%DB%92-%D9%85%D9%85%D8%A8%D8%B1%D8%A7%D9%86.59465/'
-webpage = urlreq.urlopen(URL).read().decode('utf-8')
-webstr = str(webpage)
-print(webstr)
+URL = 'https://raw.githubusercontent.com/Mohibtech/Mohib-Python/master/NLP/stopwords_UR.txt'
+
+webpage = urlopen(URL)
+stopwords_UR = webpage.read().decode("utf-8")
+
+for i,word in enumerate(stopwords_UR.split()):
+    print(i,word)
