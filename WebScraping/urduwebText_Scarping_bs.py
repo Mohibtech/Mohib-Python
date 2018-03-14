08c2e5dca6a22b09afa390bcbde06f47c67054f3
Mohib Alvi <mohibtech@gmail.com>
2018-03-14 17:52:43 +0500

    Create urduwebText_Scarping_bs.py

---
+++ WebScraping/urduwebText_Scarping_bs.py
@@ -0,0 +1,20 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+# Link of Urdu Novel Posts
+html = requests.get("https://www.urduweb.org/mehfil/threads/%D9%88%DB%81-%D8%AC%D9%88-%D9%82%D8%B1%D8%B6-%D8%B1%DA%A9%DA%BE%D8%AA%DB%92-%D8%AA%DA%BE%DB%92-%D8%AC%D8%A7%D9%86-%D9%BE%D8%B1-%D8%A7%D8%B2-%D9%81%D8%B1%D8%AD%D8%AA-%D8%A7%D8%B4%D8%AA%DB%8C%D8%A7%D9%82-%D9%85%DA%A9%D9%85%D9%84-%D9%86%D8%A7%D9%88%D9%84.6032/").text
+soup = BeautifulSoup(html, "lxml")
+
+#articles = soup.findAll("div")
+
+items = dict()
+article_rows = soup.findAll("blockquote", {"class": "messageText"})
+
+articleSoup = BeautifulSoup(str(article_rows[0]),"lxml" )
+articleText = articleSoup.get_text()
+
+# regex pattern for matching square brackets and text within it [*]
+strCleanred = re.sub("[\[].*?[\]]", "",  articleText)
+
+print(strCleaned)
