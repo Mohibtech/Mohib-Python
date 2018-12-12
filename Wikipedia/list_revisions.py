5e0b7d8430f3f61588b2234429a06a54607f56c1
Mohib Alvi <mohibtech@gmail.com>
2018-12-12 06:40:00 +0500

    Create list_revisions.py

---
+++ Wikipedia/list_revisions.py
@@ -0,0 +1,16 @@
+# This script lists top 5 user revisions 
+# Pre-requisite for this script is to Install mwclient using 
+# pip install mwclient
+
+from mwclient import Site
+site = Site('ur.wikipedia.org')
+
+
+page = site.pages['شاہ ولی اللہ']
+text = page.text()
+
+
+users = [rev['user'] for rev in page.revisions()]
+unique_users = set(users)
+user_revisions = [{'user': user, 'count': users.count(user)} for user in unique_users]
+print(sorted(user_revisions, key=lambda x: x['count'], reverse=True)[:5])
