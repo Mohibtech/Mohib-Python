837657e3162882e762455756e6b96dd516b53db5
Mohib Alvi <mohibtech@gmail.com>
2018-12-14 17:39:54 +0500

    Create InterwikiLinksDisplay.py

---
+++ Wikipedia/InterwikiLinksDisplay.py
@@ -0,0 +1,18 @@
+# This script takes Urdu Wikipedia page and display related interwiki pages in a dictionary along with languages.
+import pywikibot
+
+site = pywikibot.Site('ur', 'wikipedia')
+page = pywikibot.Page(site, u"ادیتی شرما")
+
+langlst = page.iterlanglinks()
+
+interDict = {}
+for i in langlst:
+    lang = str(i.site).split(':')[1]
+    interDict[lang] = i.title
+     
+print(interDict)
+
+# output of last print statement 
+# {'ar': 'أديتي شارما', 'en': 'Aditi Sharma', 'gu': 'અદિતિ શર્મા', 'hi': 'अदिति शर्मा', 
+# 'mr': 'आदिती शर्मा', 'pa': 'ਅਦਿਤੀ ਸ਼ਰਮਾ', 'te': 'అదితి శర్మ'}
