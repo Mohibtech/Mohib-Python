1bd82f56543cbb24769c7a741a921ac0fcf2ccf6
Mohib Alvi <mohibtech@gmail.com>
2018-12-14 17:27:28 +0500

    Update ListSplitInDictionary.py

--- short scripts/ListSplitInDictionary.py
+++ short scripts/ListSplitInDictionary.py
@@ -1,8 +1,10 @@
 # Splitting List into a dictionary while stripping unwanted characters like '[[' and ']]'
-lst = ['[[ur:ادیتی شرما]]','[[ar:أديتي شارما]]']
+lst = ['[[en:Aditi Sharma]]','[[ur:ادیتی شرما]]','[[ar:أديتي شارما]]']
 
+# Stripping '[[]]' from list element and then split the element based on character ':' to 
+# form a dictionary element based on language code and related  
 langdict  = dict(i.strip('[[]]').split(':') for i in lst)
 print(langdict)
 
 # Output
-# {'ur': 'ادیتی شرما', 'ar': 'أديتي شارما'}
+{'en': 'Aditi Sharma', 'ur': 'ادیتی شرما', 'ar': 'أديتي شارما'}
