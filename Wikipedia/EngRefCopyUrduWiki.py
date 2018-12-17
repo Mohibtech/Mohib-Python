f3c7f5595dd71ee9b50e91a3a73b3d71558e786c
Mohib Alvi <mohibtech@gmail.com>
2018-12-18 01:03:32 +0500

    Update EngRefCopyUrduWiki.py
    Extracted all references using filter_tags function of mwparserfromhell library for matching exact reference ordering in reference list which appears at the end of the article under REFERENCES.

--- Wikipedia/EngRefCopyUrduWiki.py
+++ Wikipedia/EngRefCopyUrduWiki.py
@@ -1,23 +1,56 @@
 # Copy Reference from English Article to Urdu Page
+import mwparserfromhell as mwp
 import pywikibot
 
-site = pywikibot.Site()
-page = pywikibot.Page(site, u"Aditi Sharma")
-
-refdict = {}
-reflinks = page.extlinks()
-for i in range(10):
-    refdict[i] = '<ref>' + str(next(reflinks)) + '</ref>'
-
-for num in range(5):
-    print(refdict[num]) 
+title = "داس کیپیٹل"
 
 site = pywikibot.Site('ur', 'wikipedia')
-page = pywikibot.Page(site, u"ادیتی شرما")
-#print(page.text)
+urpage = pywikibot.Page(site, title)
 
-# Adding Reference at the end of the page.
-page.text = page.text + "\n" + refdict[3]
-print(page.text)
+langlst = urpage.iterlanglinks()
 
-#page.save(u"انگریزی حوالے کاپی کیے")
+interDict = {}
+for i in langlst:
+    lang = str(i.site).split(':')[1]
+    interDict[lang] = i.title
+
+if len(interDict)==0:
+    print('Link Dictionary is empty')
+else:
+    englink = interDict['en']
+
+    site = pywikibot.Site('en', 'wikipedia')
+    enpage = pywikibot.Page(site, englink)
+
+    wikitext = enpage.get()               
+    wikicode = mwp.parse(wikitext)
+
+    # Extracting External Links using mwparserfromhell function
+    extlinks = wikicode.filter_external_links()
+
+    alltags = wikicode.filter_tags()
+    reftags = [tag.contents for tag in alltags if tag.tag=='ref']
+
+    dlinks = {}
+    for  i,link in enumerate(reftags):
+        i+=1
+        dkey = 'ح' + str(i) + 'ح'
+        dlinks[dkey] = '<ref>' + str(link) + '</ref>'
+
+    urtext = urpage.text
+    for r in tuple(dlinks.items()):
+        urtext = urtext.replace(*r)
+
+    newln = '\n'
+
+    hawalajat = '{{حوالہ جات}}'
+    urduref = '== حوالہ جات ==' + newln + hawalajat + newln
+    if hawalajat not in urtext:
+        urpage.text = urtext + newln*2 + urduref + newln
+    else:
+        urpage.text = urtext + newln*2
+
+    print('Printing appended Urdu Page')
+    print(urpage.text)
+
+    urpage.save(u"انگریزی حوالے کاپی کیے")
