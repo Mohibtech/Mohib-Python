cdc0bc15a7c7b9419b588622da4eeb9fa0c70b95
Mohib Alvi <mohibtech@gmail.com>
2018-12-29 01:47:05 +0500

    Update copyLinks_pwb_template.py
    Added import noreferences

--- Wikipedia/copyLinks_pwb_template.py
+++ Wikipedia/copyLinks_pwb_template.py
@@ -15,6 +15,7 @@ from __future__ import absolute_import, division, unicode_literals
 
 import pywikibot
 import mwparserfromhell as mwp
+from scripts import noreferences
 from pywikibot import pagegenerators
 
 from pywikibot.bot import (
